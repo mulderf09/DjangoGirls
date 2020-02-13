0b322f5f8ac811b5ebc718e485ce490b42b9e264
mulderf09 <mulderf09@gmail.com>
2020-02-13 07:01:35 +0000

    Added views to create/edit blog post inside the site

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
